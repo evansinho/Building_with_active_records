b6cb61e862eacef555dff3ef8227c7f719137941
imahnama <muannama25@gmail.com>
2020-05-28 18:34:13 +0300

    add comments model/migration and its association to user and post tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_140555) do
+ActiveRecord::Schema.define(version: 2020_05_28_151650) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "post_author"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
